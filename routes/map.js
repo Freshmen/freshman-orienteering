a1c62c562e679933ad4731e03d93cfed32e13cfe
Tuomo Vierros <tuomo.vierros@gmail.com>
2012-10-29 01:25:42 +0200

    Fixed the titles in map views

--- routes/map.js
+++ routes/map.js
@@ -4,14 +4,17 @@
 
 var providers = {
 	'GoogleMaps' : {
+			'title' : 'Google Maps',
 			'api': 'https://maps.googleapis.com/maps/api/js?sensor=true',
 			'js' : '/javascripts/googleMaps.js'
 	},
 	'NokiaMaps' : {
+			'title' : 'Nokia Maps',
 			'api': 'http://api.maps.nokia.com/2.2.3/jsl.js',
 			'js' : '/javascripts/nokiaMaps.js'
 	},
 	'OpenStreetMaps': {
+			'title' : 'openStreetMap',
 			'api': 'http://www.openlayers.org/api/OpenLayers.js',
 			'js' : '/javascripts/openStreetMap.js'
 	}
