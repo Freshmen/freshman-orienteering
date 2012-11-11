66f62e9f9e77e8a982a91e106e264094a25d86b6
Tuomo Vierros <tuomo.vierros@gmail.com>
2012-11-11 15:22:38 +0200

    made the db.js respond with 'ok'

--- routes/db.js
+++ routes/db.js
@@ -2,4 +2,5 @@ exports.create = function(req, res){
 	//create a couch, this will only create once and all the rest will be blocked by CouchDB
 	var nano = require('nano')('http://fori.uni.me:8124');
 	var message = nano.db.create('fori');
+	res.send('ok');
 };
