8f6ab4ccb3c39ce4b6a79313252ecd2cb60bae49
juuli <jukkasaarelma@gmail.com>
2012-11-11 15:30:58 +0200

    tiny change in the fbLogin

--- public/facebookTryout/fbLogin.js
+++ public/facebookTryout/fbLogin.js
@@ -36,7 +36,7 @@
      var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
      if (d.getElementById(id)) {return;}
      js = d.createElement('script'); js.id = id; js.async = true;
-     js.src = "http://connect.facebook.net/en_US/all" + (debug ? "/debug" : "") + ".js";
+     js.src = "//connect.facebook.net/en_US/all" + (debug ? "/debug" : "") + ".js";
      ref.parentNode.insertBefore(js, ref);
    }(document, /*debug*/ false));
   // Load the SDK Asynchronously
