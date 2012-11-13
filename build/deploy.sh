1da8c45498b3ae24ca90e8f8bb3ca85649d6ce54
Tuomo Vierros <tuomo.vierros@gmail.com>
2012-11-14 00:20:11 +0200

    modified build script

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 npm install
 ./node_modules/forever/bin/forever stop app.js
-./node_modules/forever/bin/forever -l ./build_$1.log -o ./build_$1.out -e ./build_$1.err start app.js
+./node_modules/forever/bin/forever -o ./build_$1.out -e ./build_$1.err start app.js
