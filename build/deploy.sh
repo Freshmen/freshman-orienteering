e5912a2e597cdfcb01f9bc2b1c1b8b6dc41e3302
Tuomo Vierros <tuomo.vierros@gmail.com>
2012-11-15 18:56:54 +0200

    changed the build script to just restart the forever job

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 npm install
-./node_modules/forever/bin/forever stop app.js
-./node_modules/forever/bin/forever -o ./build_$1.out -e ./build_$1.err start app.js
+./node_modules/forever/bin/forever restart app.js
