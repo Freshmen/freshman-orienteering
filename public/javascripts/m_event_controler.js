baed012d4e71b9995be831c0c820a85ab7b41076
Ken <wsd.2012.ken@gmail.com>
2012-11-22 20:45:18 +0200

    cope with Jukka: Create a view page for individual waypoints

--- public/javascripts/m_event_controler.js
+++ public/javascripts/m_event_controler.js
@@ -2,6 +2,7 @@ $().ready(function(){
 	var isDisplay = false;
 	var defaultColor = '#333';
 	function displayEventList(){
+		injectEvents();
 		$('#listContent').css('display','block');
 		$('#testEvent a').css('color','#44e');
 		isDisplay = true;
@@ -11,12 +12,36 @@ $().ready(function(){
 		$('#testEvent a').css('color',defaultColor);
 		isDisplay = false;
 	}
+	function injectEvents(){
+//		var eventName = $(this).children().text().replace(/ /,"");
+		// new data
+		$.ajax({
+			  url: '/mockData/eventExample.json',
+			  success: function(data) {
+				// inject
+				  new EJS({url: 'mockData/mobileList.ejs'}).update('contentWrap', {content: data.db});
+			  }
+			});
+			
+	}
 	$('#testEvent').click(function(){
 		if(!isDisplay){
 			displayEventList();
 		}else{
 			undisplayEventList();
 		}
+		
+	});
+	$('#eventList li').live('click',function(){
+		var eventName = $(this).children().text().replace(/ /,"");
+		// new data
+		$.ajax({
+			  url: '/mockData/' + eventName + '.json',
+			  success: function(data) {
+				// inject
+				var callback = new EJS({url: '/mockData/mobileList.ejs'}).update('contentWrap',{content:data.checkpoints});
+			  }
+			});
 		
 	});
 });
