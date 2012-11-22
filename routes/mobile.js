7f2f7e8831c74c3195456989ef964ecb1baef1e6
juuli <jukkasaarelma@gmail.com>
2012-11-22 18:57:48 +0200

    Change in the mobile.ejs list

--- routes/mobile.js
+++ routes/mobile.js
@@ -3,7 +3,7 @@
 var content = {};
 
 var fs = require('fs');
-fs.readFile('./temp/testData.json', 'utf8', function(err, data){
+fs.readFile('./temp/eventExample.json', 'utf8', function(err, data){
 	if(err){
 		return console.log(err);
 	}
@@ -16,7 +16,7 @@ exports.show = function(req, res){
 	//res.render('mobile', { 'selection' : events[req.query.selection] || data['Index']})
 	res.render('mobile', { 'title' : 'Freshman Orienteering',
 							'signinStatus' : 'Sign In',
-							'content' : content.events});
+							'content' : content.db});
 }
 
 /*
