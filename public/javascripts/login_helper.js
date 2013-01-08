11aa5fd07f65de06dfceaeee741f2fb9d77945b1
juuli <jukkasaarelma@gmail.com>
2013-01-08 16:32:43 +0200

    Updated the template path in login_helper.js

--- public/javascripts/login_helper.js
+++ public/javascripts/login_helper.js
@@ -2,9 +2,9 @@ function updateTemplate_Login(){
 	var o = this;
 	if (typeof o.response === "undefined"){
 		//not logged in
-		return new EJS({url: '/mockData/login_status.ejs'}).update('contentWrap', {});
+		return new EJS({url: '/templates/login_status.ejs'}).update('contentWrap', {});
 	}else{
 		//loged in
-		return new EJS({url: '/mockData/login_status.ejs'}).update('contentWrap', {content: o});
+		return new EJS({url: '/templates/login_status.ejs'}).update('contentWrap', {content: o});
 	}
 }
