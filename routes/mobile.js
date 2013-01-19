2348595e44c7d7ae37d0c933a7bed48fc505d69d
Ken <wsd.2012.ken@gmail.com>
2013-01-19 17:19:37 +0200

    bypass login

--- routes/mobile.js
+++ routes/mobile.js
@@ -13,8 +13,7 @@ fs.readFile('./temp/eventExample.json', 'utf8', function(err, data){
 
 exports.show = function(req, res){
 	res.render('mobile', { 'title' : 'Freshman Orienteering',
-							'user' : req.user,
-							'content' : content.db});
+							'user' : req.user?req.user:{name : 'Sign in'}});
 }
 
 /*
