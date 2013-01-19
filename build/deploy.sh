125559e95a9033a7981af69abd354221e655c014
Tuomo Vierros <tuomo.vierros@gmail.com>
2013-01-20 00:13:23 +0200

    added log files to build script

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 npm install
 ./node_modules/forever/bin/forever stop app.js
-./node_modules/forever/bin/forever start app.js
+./node_modules/forever/bin/forever start app.js -o out_$1.log -e err_$1.log -l forever_$1.log
