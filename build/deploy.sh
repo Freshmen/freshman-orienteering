40fd25009fbec4f3f15a56584e89d735b7229e29
Tuomo Vierros <tuomo.vierros@gmail.com>
2013-01-23 01:33:42 +0200

    changed build script to not be killed by jenkins

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,4 +1,5 @@
+
 #!/bin/bash
 npm install
 ./node_modules/forever/bin/forever stop app.js
-./node_modules/forever/bin/forever start -o out_$1.log -e err_$1.log -l forever_$1.log app.js 
+BUILD_ID=daemon ./node_modules/forever/bin/forever start -o out_$1.log -e err_$1.log -l forever_$1.log app.js 
