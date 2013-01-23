8622d25b1b489bae6361a9e3c20ca87b3268add0
Tuomo Vierros <tuomo.vierros@gmail.com>
2013-01-23 02:02:05 +0200

    reverted build script

--- build/deploy.sh
+++ build/deploy.sh
@@ -3,4 +3,4 @@
 npm install
 ./node_modules/forever/bin/forever stop app.js
 mkdir -p logs
-BUILD_ID=daemon ./node_modules/forever/bin/forever start -l $PWD/logs/forever_$1.log app.js 
+BUILD_ID=daemon ./node_modules/forever/bin/forever start -l forever_$1.log app.js 
