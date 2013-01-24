405b158a2482fb0aca2c340696fd752398bcbb00
Tuomo Vierros <tuomo.vierros@gmail.com>
2013-01-24 16:25:35 +0200

    jenkins building with facebook works now, took out the console.log

--- build/deploy.sh
+++ build/deploy.sh
@@ -3,5 +3,4 @@
 npm install
 ./node_modules/forever/bin/forever stop app.js
 mkdir -p logs
-echo $FACEBOOK_APP_ID
 BUILD_ID=daemon ./node_modules/forever/bin/forever start -l "${PWD}/logs/forever_$1.log" app.js 
