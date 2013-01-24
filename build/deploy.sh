b6e06dd0de48bab9a638be6d93a2db6b96284711
Tuomo Vierros <tuomo.vierros@gmail.com>
2013-01-24 15:50:54 +0200

    changed build script to log the env variable

--- build/deploy.sh
+++ build/deploy.sh
@@ -3,4 +3,5 @@
 npm install
 ./node_modules/forever/bin/forever stop app.js
 mkdir -p logs
+echo $FACEBOOK_APP_ID
 BUILD_ID=daemon ./node_modules/forever/bin/forever start -l "${PWD}/logs/forever_$1.log" app.js 
