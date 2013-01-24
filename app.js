e3145ef7791f5dd6b86bb49922697604590523d8
Ken <wsd.2012.ken@gmail.com>
2013-01-24 19:41:49 +0200

    refine events

--- app.js
+++ app.js
@@ -126,8 +126,8 @@ app.configure('development', function(){
 app.get('/', routes.show);
 app.get('/desktop', desktop.show);
 app.get('/desktop_create', create.show);
-//app.get('/mobile',ensureAuthenticated, mobile.show);
-app.get('/mobile', ensureAuthenticated, mobile.show);
+app.get('/mobile', mobile.show);
+//app.get('/mobile', ensureAuthenticated, mobile.show);
 app.get('/login', login.show);
 //GET /auth/facebook
 //Use passport.authenticate() as route middleware to authenticate the
