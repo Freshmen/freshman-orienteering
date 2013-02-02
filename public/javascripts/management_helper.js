da6bad56487e8614e6fddf4aa08dd58ed18d1b3e
Raoul Udd <raoul.udd@gmail.com>
2013-02-02 19:11:39 +0200

    added editing of event name and deletion of events

--- public/javascripts/management_helper.js
+++ public/javascripts/management_helper.js
@@ -9,7 +9,7 @@ function getEventData(){
 
 function getUserEvents(){
 	$.getJSON("/api/v2/me", function(user) {
-		 var userID = user._id;
+		var userID = user._id;
 		if (log == 1){
 			console.log(userID);
 		}
@@ -32,33 +32,36 @@ function populatePage(eventData){
 		$('#event_'+currentID).append('<h3 class="eventNameTag">'+eventData[e].title+'</h3>');
 		$('#event_'+currentID).append('<input type="button" id="detailsButton" onclick=viewEventDetails("'+e+'") value="Details" class="button">');
 		$('#event_'+currentID).append('<input type="button" id="submissionsButton" onclick=viewSubmissions("'+e+'") value="Submissions" class="button">');
-
 	}
 	$('#ajaxLoader').fadeOut("fast");
 	$('footer').append('<a href="#">View old events...</a>');
 }
 
-function viewEventDetails(eventID){
-	var thisEvent = userEvents[eventID];
+function viewEventDetails(eventNumber){
+	var thisEvent = userEvents[eventNumber];
 	var starttime = new Date(thisEvent.starttime);
 	var endtime = new Date(thisEvent.endtime);
+	var editLink = '<a href="#" onclick=editProperty(this,"'+thisEvent._id+'") class="propertyEditLink">(edit)</a>';
+	if (log == 1){
+				console.log(thisEvent._id);
+				//console.log(editLink);
+			}
 	clearContent($('#content'),"&lt; Back");
 	$('#content').append('<div id="eventDetailsHolder"></div>');
-	$('#eventDetailsHolder').append('<h1>'+thisEvent.title+'</h2>');
+	$('#eventDetailsHolder').append('<h1 id="title" class="propertyHeader">'+thisEvent.title+'</h1>'+editLink);
 	$('#eventDetailsHolder').append('<h3>Location</h3>');
-	$('#eventDetailsHolder').append('Latitude: '+thisEvent.location.latitude+'<br />Longitude: '+thisEvent.location.longitude);
-	$('#eventDetailsHolder').append('<h3>Time</h3>');
-	$('#eventDetailsHolder').append('The event starts at '+starttime+' and ends at '+endtime);
-	$('#eventDetailsHolder').append('<h3>Description</h3>');
-	$('#eventDetailsHolder').append(thisEvent.description);
+	$('#eventDetailsHolder').append('<p>Latitude: '+thisEvent.location.latitude+'</p><p>Longitude: '+thisEvent.location.longitude+'</p>');
+	$('#eventDetailsHolder').append('<h3 class="propertyHeader">Time</h3>');
+	$('#eventDetailsHolder').append('<p>The event starts at '+starttime+' and ends at '+endtime+'</p>');
+	$('#eventDetailsHolder').append('<h3 id="description" class="propertyHeader">Description</h3>');
+	$('#eventDetailsHolder').append('<p>'+thisEvent.description+'</p>');
 	if (thisEvent.ordered == 'true'){
 		$('#eventDetailsHolder').append('<h5>The checkpoints of this event are ordered</h5>');
 	}
 	else {
 		$('#eventDetailsHolder').append('<h5>The checkpoints of this event are unordered</h5>');
 	}
-
-
+	$('#eventDetailsHolder').append('<input type="button" id="deleteButton" onclick=askConfirmation("'+thisEvent._id+'") value="Delete this event" class="button">');
 }
 
 function viewSubmissions(eventID){
@@ -67,10 +70,65 @@ function viewSubmissions(eventID){
 	$('#content').append('<div id="submissionsHolder"></div>');
 }
 
+function editProperty(link, ID){
+	var editee = $(link).prev();
+	if ($(link).text() == '(edit)'){
+		editee.replaceWith('<input type="text" id="'+editee.attr("id")+'" class="'+editee.attr("class")+'" value="'+editee.text()+'">');
+		$(link).text('(save)');
+	}
+	else {
+		var obj = {};
+		obj[editee.attr("id")] = editee.val();
+		$.ajax({
+			url:'/api/v2/events/'+ID,
+			type:'PUT',
+			data: obj,
+			success: function(response,data){}
+		});
+		editee.replaceWith('<h1 type="text" id="'+editee.attr("id")+'" class="'+editee.attr("class")+'">'+editee.val()+'</h1>');
+		$(link).text('(edit)');
+	}
+}
+
 function clearContent(elementToClear,newFooter){
 	elementToClear.empty();
 	$('footer').empty();
 	$('footer').append('<a href="desktop_manage">'+newFooter+'</a>');
 }
 
+function askConfirmation(eventID){
+	var notifier = new Backbone.Notifier({
+		theme: 'plastic',
+		modal: true,
+		position: 'center',
+		zIndex: 10000,
+		screenOpacity: 0.7,
+	});
 
+	var confirmMsg = notifier.notify({
+	message: "Are you sure you want to delete this event?",
+	'type': "error",
+	buttons: [
+		{'data-role': 'ok', text: 'Yes'},
+		{'data-role': 'cancel', text: 'No', 'class': 'default'}
+	],
+	modal: true,
+	ms: null,
+	destroy: false
+})
+.on('click:ok', function(){
+	this.destroy();
+	deleteEvent(eventID);
+	notifier.success('The event has been deleted.');
+})
+.on('click:cancel', 'destroy');
+}
+
+function deleteEvent(eventID){
+	$.ajax({
+		url:'/api/v2/events/'+eventID,
+		type:'DELETE',
+		success: function(response,data){}
+	});
+	window.location.reload()
+}
