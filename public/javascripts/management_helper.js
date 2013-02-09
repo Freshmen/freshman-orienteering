b56b8be5ddf9a33f365210af7f195c00c6ab561a
Raoul Udd <raoul.udd@gmail.com>
2013-02-09 16:48:14 +0200

    Worked on submissions table

--- public/javascripts/management_helper.js
+++ public/javascripts/management_helper.js
@@ -71,11 +71,8 @@ function viewSubmissions(eventID){
 	var thisEvent = userEvents[eventID];
 	clearContent($('#content'),"&lt; Back");
 	$('#content').append('<div id="submissionsHolder"></div>');
-	$('#submissionsHolder').append('<table id="submissions"><thead><tr><th>Submission</th><th>Time</th><th>Checkpoint</th><th>Submitter</th><th>Group</th><th>Rating</th><th>Share</th></tr></thead></table>');
+	$('#submissionsHolder').append('<table id="submissions" class="tablesorter"><thead><tr><th>Submission</th><th>Time</th><th>Checkpoint</th><th>Submitter</th><th>Group</th><th>Rating</th><th>Share</th></tr></thead><tbody></tbody></table>');
 	$('#submissions').tablesorter();
-
-	var row = document.getElementById("submissions").insertRow(0);
-	row.insertCell(0).innerHTML="test";
 
 }
 
