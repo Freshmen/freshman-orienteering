d213460ffda6bebf9f0a39b6df1b5ab3fcf292ca
Tuomo Vierros <tuomo.vierros@gmail.com>
2013-02-14 17:36:32 +0200

    minor styling fixes

--- routes/desktop.js
+++ routes/desktop.js
@@ -1,22 +1,20 @@
-
 /*
  * GET desktop page.
  */
 
-exports.show = function(req, res){
+exports.show = function(req, res) {
   res.render('desktop', { title: 'Gamified : Desktop Page', user : req.user });
 };
 
-exports.create = function(req, res){
+exports.create = function(req, res) {
   res.render('desktop_create', { title: 'Gamified : Create Event', user: req.user });
 };
 
-exports.manage = function(req, res){
-  res.render('desktop_manage', { title: 'Gamified : Manage Event', user: req.user });
+exports.manage = function(req, res) {
+	res.render('desktop_manage', { title: 'Gamified : Manage Event', user: req.user, events: events });
 };
 
 exports.details = function(req, res) {
-	var currentEvent;
 	res.render();
 }
 
